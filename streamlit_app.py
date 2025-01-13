f4a7542b2bdfbc66e82776bd3ef8b01ec5cdfe88
Lali-cell12 <125992481+Lali-cell12@users.noreply.github.com>
2025-01-13 22:38:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,9 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!,MellyMel', icon="✅")
 
-
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 
 
